603b9d49f1c90d36690cb72b7964c349aa6e61ee
Renan <renansalmazio@gmail.com>
2023-09-17 10:49:43 -0300

    arquivos ruby

---
+++ primitivos.rb
@@ -0,0 +1,14 @@
+# tipo primitivos
+
+# string
+palavras = "texto".class
+puts palavras
+
+numeroInteiro  = 3.class
+puts numeroInteiro
+
+numeroFlutuante  = 3.5.class
+puts numeroFlutuante
+
+boleano  = false.class
+puts boleano
