1f1c63ca260d58f0842f2fd10d64a049d995b9c2
Renan Salmazio <renansalmazio@gmail.com>
2023-09-28 16:54:58 +0000

    arquivos

---
+++ operadoresLogicos.rb
@@ -0,0 +1,15 @@
+v1 = 34 
+v2 = 56 
+v3 = 2
+v4  = 7
+if (v1 < v2) && (v3 < v4)  then
+    puts  "codição aceita nos dois casos" 
+end
+
+if (v1 < v2) || (v3 < v4)  then
+    puts  "codição aceita nos dois casos" 
+end
+
+if !(v1 < v2) &&  !(v3 < v4)  then
+    puts  "codição aceita nos dois casos" 
+end
